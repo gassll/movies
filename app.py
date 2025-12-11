74447271f214c6a7933192100e1ab1cab8105448
gassll <lizakonopatskaya@gmail.com>
2025-12-11 19:36:12 +0300

    убрала логику из app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from logic import load_movies, add_movie, save_movies, mark_watched, find_by_year
+from logic import load_movies, add_movie, save_movies, mark_watched, find_by_year, action_find_by_year, \
+    action_mark_watched, action_add_movie, action_load_movies
 
 DATA_FILE = "movies.json"
 
@@ -16,69 +17,17 @@ def main():
         choice = input("Выберите пункт: ")
 
         if choice == "1":
-            if not movies:
-                print("Список фильмов пуст.")
-            else:
-                print("\nСписок фильмов:")
-                for movie in movies:
-                    status = "Просмотрен" if movie["watched"] else "Не просмотрен"
-                    print(f"[{movie['id']}] {movie['title']} ({movie['year']}) — {status}")
+            action_load_movies(movies)
         elif choice == "2":
-            title = input("Введите название фильма: ").strip()
-            year_raw = input("Введите год: ").strip()
-
-            if not year_raw.isdigit():
-                print("Ошибка: год должен быть числом.")
-                continue
-
-            year = int(year_raw)
-
-            add_movie(movies, title, year)
-            save_movies(DATA_FILE, movies)
-
-            print("Фильм успешно добавлен!")
-
+            action_add_movie(movies)
         elif choice == "3":
-            movie_id_raw = input("Введите ID фильма: ").strip()
-
-            if not movie_id_raw.isdigit():
-                print("Ошибка: ID должен быть числом.")
-                continue
-
-            movie_id = int(movie_id_raw)
-
-            exists = any(movie.get("id") == movie_id for movie in movies)
-            if not exists:
-                print("Фильм с таким ID не найден.")
-                continue
-
-            mark_watched(movies, movie_id)
-            save_movies(DATA_FILE, movies)
-            print("Фильм отмечен как просмотренный!")
-
+            action_mark_watched(movies)
         elif choice == "4":
-            year_raw = input("Введите год для поиска: ").strip()
-
-            if not year_raw.isdigit():
-                print("Ошибка: год должен быть числом.")
-                continue
-
-            year = int(year_raw)
-            results = find_by_year(movies, year)
-
-            if not results:
-                print(f"Фильмов за {year} год не найдено.")
-            else:
-                print(f"Фильмы за {year} год:")
-                for movie in results:
-                    status = "Просмотрен" if movie["watched"] else "Не просмотрен"
-                    print(f"[{movie['id']}] {movie['title']} ({movie['year']}) — {status}")
-
+            action_find_by_year(movies)
         elif choice == "0":
             save_movies(DATA_FILE, movies)
             print("До свидания!")
             break
-
         else:
             print("Неверный пункт меню.")
 
--- logic.py
+++ logic.py
@@ -1,6 +1,15 @@
 import json
 import os
+DATA_FILE = "movies.json"
 
+def action_load_movies(movies):
+    if not movies:
+        print("Список фильмов пуст.")
+    else:
+        print("\nСписок фильмов:")
+        for movie in movies:
+            status = "Просмотрен" if movie["watched"] else "Не просмотрен"
+            print(f"[{movie['id']}] {movie['title']} ({movie['year']}) — {status}")
 
 def load_movies(path: str) -> list[dict]:
     if not os.path.exists(path):
@@ -21,6 +30,21 @@ def save_movies(path: str, movies: list[dict]) -> None:
         json.dump(movies, f, ensure_ascii=False, indent=4)
 
 
+def action_add_movie(movies: list[dict]) -> None:
+    title = input("Введите название фильма: ").strip()
+    year_raw = input("Введите год: ").strip()
+
+    if not year_raw.isdigit():
+        print("Ошибка: год должен быть числом.")
+        return
+
+    year = int(year_raw)
+
+    add_movie(movies, title, year)
+    save_movies(DATA_FILE, movies)
+
+    print("Фильм успешно добавлен!")
+
 def add_movie(movies: list[dict], title: str, year: int) -> list[dict]:
     if movies:
         max_id = max(movie.get('id', 0) for movie in movies)
@@ -37,13 +61,50 @@ def add_movie(movies: list[dict], title: str, year: int) -> list[dict]:
     movies.append(new_movie)
     return movies
 
+def action_mark_watched(movies: list[dict]) -> list[dict]:
+    movie_id_raw = input("Введите ID фильма: ").strip()
+
+    if not movie_id_raw.isdigit():
+        print("Ошибка: ID должен быть числом.")
+        return
+
+    movie_id = int(movie_id_raw)
+
+    exists = any(movie.get("id") == movie_id for movie in movies)
+    if not exists:
+        print("Фильм с таким ID не найден.")
+        return
+
+    mark_watched(movies, movie_id)
+    save_movies(DATA_FILE, movies)
+    print("Фильм отмечен как просмотренный!")
+
 def mark_watched(movies: list[dict], movie_id: int) -> list[dict]:
     for movie in movies:
         if movie.get("id") == movie_id:
             movie["watched"] = True
-
+            break
 
     return movies
+
+def action_find_by_year(movies: list[dict]) -> list[dict]:
+        year_raw = input("Введите год для поиска: ").strip()
+
+        if not year_raw.isdigit():
+            print("Ошибка: год должен быть числом.")
+            return
+
+        year = int(year_raw)
+        results = find_by_year(movies, year)
+
+        if not results:
+            print(f"Фильмов за {year} год не найдено.")
+        else:
+            print(f"Фильмы за {year} год:")
+            for movie in results:
+                status = "Просмотрен" if movie["watched"] else "Не просмотрен"
+                print(f"[{movie['id']}] {movie['title']} ({movie['year']}) — {status}")
+
 
 def find_by_year(movies: list[dict], year: int) -> list[dict]:
     return [movie for movie in movies if movie.get("year") == year]
